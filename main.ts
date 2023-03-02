69e3a6a110571106713319e645a3c717e6b45f32
kZKz1021 <126796182+kZKz1021@users.noreply.github.com>
2023-03-02 23:04:59 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,37 @@
+function back90run () {
+    wuKong.setAllMotor(-100, -100)
+    basic.pause(1000)
+    wuKong.setAllMotor(80, 0)
+    basic.pause(500)
+    wuKong.setAllMotor(80, 80)
+    basic.pause(1000)
+}
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+strip.show()
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1 || pins.digitalReadPin(DigitalPin.P2) == 1) {
+        back90run()
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    } else if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P8) > 0 && sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P8) <= 15) {
+        wuKong.setAllMotor(100, 100)
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+    } else if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P8) > 15 && sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P8) <= 30) {
+        wuKong.setAllMotor(100, 100)
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+        strip.show()
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 0 || pins.digitalReadPin(DigitalPin.P2) == 0) {
+        wuKong.setAllMotor(100, 0)
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else {
+        wuKong.setAllMotor(100, 0)
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        strip.show()
+    }
+})
